8bfe06c82f78f9651176c0000a2a7216017d655a
Ray Jacobson <ray@audius.co>
2025-10-05 13:34:34 -0700

    Add full artist coins terms page/link (#13158)
    So it matches existing terms pages and PDF can change
    
    _Please describe the tests that you ran to verify your changes. Provide
    repro instructions & any configuration._
    
    http://localhost:3002/legal/artist-coin-terms

--- packages/common/src/utils/route.ts
+++ packages/common/src/utils/route.ts
@@ -9,6 +9,7 @@ import { convertGenreLabelToValue, Genre } from './genres'
 // External Routes
 export const PRIVACY_POLICY = '/legal/privacy-policy'
 export const TERMS_OF_SERVICE = '/legal/terms-of-use'
+export const ARTIST_COIN_TERMS = '/legal/artist-coin-terms'
 export const API_TERMS = '/legal/api-terms'
 export const DOWNLOAD_START_LINK = '/download?start_download=true'
 export const DOWNLOAD_LINK = '/download'
@@ -208,6 +209,8 @@ export const AUDIUS_DEV_STAKER_LINK = 'https://audius.org/protocol'
 export const AUDIUS_HOT_AND_NEW =
   '/audius/playlist/hot-new-on-audius-%F0%9F%94%A5-4281'
 export const AUDIUS_HELP_LINK = 'https://help.audius.co/'
+export const AUDIUS_ARTIST_COIN_HELP_LINK =
+  'https://help.audius.co/product/artist-coins'
 
 export const AUDIUS_CAREERS_LINK = 'https://www.tikilabs.com/careers'
 export const AUDIUS_PODCAST_LINK =
@@ -256,6 +259,7 @@ export const publicSiteRoutes = [
   TERMS_OF_SERVICE,
   PRIVACY_POLICY,
   API_TERMS,
+  ARTIST_COIN_TERMS,
   DOWNLOAD_LINK,
   AUTH_REDIRECT
 ]
