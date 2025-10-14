1eece57551c31a05207075f921c95b56c2dd596a
Alec Savvy <alecsavvy@outlook.com>
2025-10-14 10:44:24 -0600

    rename

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -20,14 +20,14 @@ printf "${GREEN}Applying patches...\n${NC}"
 npm run patch-package > /dev/null
 
 # xcodebuild may exist (e.g. if xcode-select is installed via homebrew) but won't work alone
-if [[ -z "${SKIP_POD_INSTALL}" ]]; then
+if [[ -z "${SKIP_MOBILE_DEPS}" ]]; then
   if ! xcodebuild --help &>/dev/null; then
     printf "${YELLOW}WARNING: Xcode not installed. Skipping mobile dependency installation.${NC}\n"
-    SKIP_POD_INSTALL=true
+    SKIP_MOBILE_DEPS=true
   fi
 fi
 
-if [[ -z "${SKIP_POD_INSTALL}" ]]; then
+if [[ -z "${SKIP_MOBILE_DEPS}" ]]; then
   printf "${GREEN}Installing cocoapods...\n${NC}"
   {
     cd ./packages/mobile/ios
@@ -42,7 +42,7 @@ if [[ -z "${SKIP_POD_INSTALL}" ]]; then
   } > /dev/null
 fi
 
-if [[ -z "${SKIP_POD_INSTALL}" ]]; then
+if [[ -z "${SKIP_MOBILE_DEPS}" ]]; then
   if command -v java >/dev/null; then
     {
       printf "${GREEN}Setting up Android dependencies...\n${NC}"
