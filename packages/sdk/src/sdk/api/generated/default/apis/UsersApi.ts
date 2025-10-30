e758e842d1182b88e7db488571f88a0b4e046ba8
Dylan Jeffers <dylan@audius.co>
2025-10-30 13:49:55 -0700

    A few sdk changes

--- packages/sdk/src/sdk/api/generated/default/apis/UsersApi.ts
+++ packages/sdk/src/sdk/api/generated/default/apis/UsersApi.ts
@@ -18,6 +18,7 @@ import * as runtime from '../runtime';
 import type {
   AlbumsResponse,
   AuthorizedApps,
+  BalanceHistoryResponse,
   CollectiblesResponse,
   ConnectedWalletsResponse,
   DeveloperApps,
@@ -56,6 +57,8 @@ import {
     AlbumsResponseToJSON,
     AuthorizedAppsFromJSON,
     AuthorizedAppsToJSON,
+    BalanceHistoryResponseFromJSON,
+    BalanceHistoryResponseToJSON,
     CollectiblesResponseFromJSON,
     CollectiblesResponseToJSON,
     ConnectedWalletsResponseFromJSON,
@@ -318,6 +321,16 @@ export interface GetTracksByUserRequest {
 
 export interface GetUserRequest {
     id: string;
+}
+
+export interface GetUserBalanceHistoryRequest {
+    id: string;
+    startTime?: Date;
+    endTime?: Date;
+    granularity?: GetUserBalanceHistoryGranularityEnum;
+    userId?: string;
+    encodedDataMessage?: string;
+    encodedDataSignature?: string;
 }
 
 export interface GetUserByHandleRequest {
@@ -1610,6 +1623,61 @@ export class UsersApi extends runtime.BaseAPI {
 
     /**
      * @hidden
+     * Get the user\'s historical portfolio balance data
+     */
+    async getUserBalanceHistoryRaw(params: GetUserBalanceHistoryRequest, initOverrides?: RequestInit | runtime.InitOverrideFunction): Promise<runtime.ApiResponse<BalanceHistoryResponse>> {
+        if (params.id === null || params.id === undefined) {
+            throw new runtime.RequiredError('id','Required parameter params.id was null or undefined when calling getUserBalanceHistory.');
+        }
+
+        const queryParameters: any = {};
+
+        if (params.startTime !== undefined) {
+            queryParameters['start_time'] = (params.startTime as any).toISOString();
+        }
+
+        if (params.endTime !== undefined) {
+            queryParameters['end_time'] = (params.endTime as any).toISOString();
+        }
+
+        if (params.granularity !== undefined) {
+            queryParameters['granularity'] = params.granularity;
+        }
+
+        if (params.userId !== undefined) {
+            queryParameters['user_id'] = params.userId;
+        }
+
+        const headerParameters: runtime.HTTPHeaders = {};
+
+        if (params.encodedDataMessage !== undefined && params.encodedDataMessage !== null) {
+            headerParameters['Encoded-Data-Message'] = String(params.encodedDataMessage);
+        }
+
+        if (params.encodedDataSignature !== undefined && params.encodedDataSignature !== null) {
+            headerParameters['Encoded-Data-Signature'] = String(params.encodedDataSignature);
+        }
+
+        const response = await this.request({
+            path: `/users/{id}/balance/history`.replace(`{${"id"}}`, encodeURIComponent(String(params.id))),
+            method: 'GET',
+            headers: headerParameters,
+            query: queryParameters,
+        }, initOverrides);
+
+        return new runtime.JSONApiResponse(response, (jsonValue) => BalanceHistoryResponseFromJSON(jsonValue));
+    }
+
+    /**
+     * Get the user\'s historical portfolio balance data
+     */
+    async getUserBalanceHistory(params: GetUserBalanceHistoryRequest, initOverrides?: RequestInit | runtime.InitOverrideFunction): Promise<BalanceHistoryResponse> {
+        const response = await this.getUserBalanceHistoryRaw(params, initOverrides);
+        return await response.value();
+    }
+
+    /**
+     * @hidden
      * Gets a single user by their handle
      */
     async getUserByHandleRaw(params: GetUserByHandleRequest, initOverrides?: RequestInit | runtime.InitOverrideFunction): Promise<runtime.ApiResponse<UserResponse>> {
@@ -2292,6 +2360,14 @@ export const GetTracksByUserGateConditionEnum = {
     Token: 'token'
 } as const;
 export type GetTracksByUserGateConditionEnum = typeof GetTracksByUserGateConditionEnum[keyof typeof GetTracksByUserGateConditionEnum];
+/**
+ * @export
+ */
+export const GetUserBalanceHistoryGranularityEnum = {
+    Hourly: 'hourly',
+    Daily: 'daily'
+} as const;
+export type GetUserBalanceHistoryGranularityEnum = typeof GetUserBalanceHistoryGranularityEnum[keyof typeof GetUserBalanceHistoryGranularityEnum];
 /**
  * @export
  */
