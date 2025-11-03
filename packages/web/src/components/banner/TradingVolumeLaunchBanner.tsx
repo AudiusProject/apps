e7b56eedac046644c49fdb5e6ea8bb036c0fe846
Raymond Jacobson <ray@audius.co>
2025-11-03 14:44:47 -0500

    Add TradingVolumeLaunchBanner

---
+++ packages/web/src/components/banner/TradingVolumeLaunchBanner.tsx
@@ -0,0 +1,47 @@
+import { useCallback, useState } from 'react'
+
+import { Name } from '@audius/common/models'
+import { useDispatch } from 'react-redux'
+import { useLocalStorage } from 'react-use'
+
+import { make } from 'common/store/analytics/actions'
+
+import { CallToActionBanner } from './CallToActionBanner'
+
+const TRADING_VOLUME_BANNER_LOCAL_STORAGE_KEY =
+  'dismissTradingVolumeLaunchBanner11.03.25'
+
+const messages = {
+  pill: 'New',
+  text: 'Earn $AUDIO trading artist coins! Season 1 is live now. 🔥'
+}
+
+export const TradingVolumeLaunchBanner = () => {
+  const dispatch = useDispatch()
+  const [isDismissed, setIsDismissed] = useLocalStorage(
+    TRADING_VOLUME_BANNER_LOCAL_STORAGE_KEY,
+    false
+  )
+  const [isVisible, setIsVisible] = useState(!isDismissed)
+
+  const handleClose = useCallback(() => {
+    setIsDismissed(true)
+    setIsVisible(false)
+  }, [setIsDismissed])
+
+  const handleAccept = useCallback(() => {
+    // Reuse existing event for simplicity; no new analytics name required
+    dispatch(make(Name.BANNER_ARTIST_COINS_LAUNCH_CLICKED, {}))
+    window.open('https://season1.audius.co', '_blank')
+    handleClose()
+  }, [dispatch, handleClose])
+
+  return isVisible ? (
+    <CallToActionBanner
+      pill={messages.pill}
+      text={messages.text}
+      onAccept={handleAccept}
+      onClose={handleClose}
+    />
+  ) : null
+}
