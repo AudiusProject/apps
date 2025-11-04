dcc13b034dddc12fba50cad9125b176cd429b148
Dylan Jeffers <dylan@audius.co>
2025-11-04 15:50:47 -0800

    return instead

--- packages/common/src/store/ui/buy-sell/useBuySellSwap.ts
+++ packages/common/src/store/ui/buy-sell/useBuySellSwap.ts
@@ -88,13 +88,15 @@ export const useBuySellSwap = (props: UseBuySellSwapProps) => {
       outputMintAddress = selectedPair.quoteToken.address ?? ''
     }
 
+    if (!baseCoin?.decimals || !quoteCoin?.decimals) return
+
     handleSwap({
       inputMint: inputMintAddress,
       outputMint: outputMintAddress,
       amountUi: inputAmount,
       slippageBps: SLIPPAGE_BPS,
-      inputDecimals: baseCoin?.decimals ?? 0,
-      outputDecimals: quoteCoin?.decimals ?? 0
+      inputDecimals: baseCoin.decimals,
+      outputDecimals: quoteCoin.decimals
     })
   }
 
