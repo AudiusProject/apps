c15d674b521001293b96d3aecaf4e2b33ba85790
Dylan Jeffers <dylan@audius.co>
2025-12-03 17:08:30 -0800

    Finalize

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -49,6 +49,19 @@ const getClientAliases = () => {
  * @type {import('metro-config').MetroConfig}
  */
 const config = {
+  server: {
+    enhanceMiddleware: (middleware) => {
+      return (req, res, next) => {
+        if (req.url === '/status') {
+          // Android dev check expects this exact string
+          res.writeHead(200, { 'Content-Type': 'text/plain' })
+          res.end('packager-status:running')
+          return
+        }
+        return middleware(req, res, next)
+      }
+    }
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
