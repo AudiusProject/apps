d3bfd1223a8c58895b50dab2ba78ac0ccc22484b
Raymond Jacobson <ray@audius.co>
2026-01-12 23:17:33 -0800

    I think I got it

--- packages/web/src/ssr/worker.js
+++ packages/web/src/ssr/worker.js
@@ -11,6 +11,11 @@ import { getAssetFromKV } from '@cloudflare/kv-asset-handler'
 import manifestJSON from '__STATIC_CONTENT_MANIFEST'
 import { Toucan } from 'toucan-js'
 import { renderPage } from 'vike/server'
+try {
+  await import('../../build-ssr/server/entry.mjs')
+} catch (e) {
+  // ok in dev / when SSR build hasn't been produced yet
+}
 
 const assetManifest = JSON.parse(manifestJSON)
 
