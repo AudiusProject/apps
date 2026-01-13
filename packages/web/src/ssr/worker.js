b05ff8f95190449b942b454a4183db83e35769e0
Raymond Jacobson <ray@audius.co>
2026-01-12 23:39:37 -0800

    Actually fix lint

--- packages/web/src/ssr/worker.js
+++ packages/web/src/ssr/worker.js
@@ -12,7 +12,7 @@ import manifestJSON from '__STATIC_CONTENT_MANIFEST'
 import { Toucan } from 'toucan-js'
 import { renderPage } from 'vike/server'
 try {
-  // @ts-ignore
+  // eslint-disable-next-line import/no-unresolved
   await import('../../build-ssr/server/entry.mjs')
 } catch (e) {
   // ok in dev / when SSR build hasn't been produced yet
