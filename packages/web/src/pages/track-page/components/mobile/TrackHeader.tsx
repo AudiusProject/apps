6364c62f90b6bfe62006ee03c13718bc83ea295c
Dylan Jeffers <dylan@audius.co>
2026-01-27 22:11:54 -0700

    Fix play button translate

--- packages/web/src/pages/track-page/components/mobile/TrackHeader.tsx
+++ packages/web/src/pages/track-page/components/mobile/TrackHeader.tsx
@@ -80,6 +80,7 @@ const PlayButton = ({ disabled, playing, onPlay }: PlayButtonProps) => {
       iconLeft={playing ? IconPause : IconPlay}
       onClick={onPlay}
       fullWidth
+      translate='no'
     >
       {playing ? messages.pause : messages.play}
     </Button>
@@ -93,6 +94,7 @@ const PreviewButton = ({ playing, onPlay }: PlayButtonProps) => {
       iconLeft={playing ? IconPause : IconPlay}
       onClick={onPlay}
       fullWidth
+      translate='no'
     >
       {playing ? messages.pause : messages.preview}
     </Button>
