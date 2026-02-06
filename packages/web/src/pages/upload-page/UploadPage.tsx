d1f716f6fe647b3e9c3a61d05ebc5f626ae10225
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2026-02-05 16:30:53 -0800

    Move harmony usage out of useUpload

--- packages/web/src/pages/upload-page/UploadPage.tsx
+++ packages/web/src/pages/upload-page/UploadPage.tsx
@@ -9,9 +9,11 @@ import {
   useUploadConfirmationModal,
   TrackMetadataForUpload,
   type CollectionFormState,
-  type TrackFormState
+  type TrackFormState,
+  toastActions
 } from '@audius/common/store'
-import { IconCloudUpload } from '@audius/harmony'
+import { route } from '@audius/common/utils'
+import { IconCloudUpload, IconArrowRight } from '@audius/harmony'
 import { useDispatch, useSelector } from 'react-redux'
 import { useLocation } from 'react-router'
 
@@ -26,7 +28,9 @@ import { FinishPage } from './pages/FinishPage'
 import SelectPage from './pages/SelectPage'
 
 const { reset } = uploadActions
+const { toast } = toastActions
 const { getFormState, getUploadSuccess, getUploadError } = uploadSelectors
+const { UPLOAD_PAGE } = route
 
 const messages = {
   selectPageTitle: 'Upload Your Music',
@@ -73,7 +77,38 @@ export const UploadPage = (props: UploadPageProps) => {
     formStateFromStore ?? initialFormState
   )
 
-  const { startUpload, finishUpload } = useUpload()
+  const handleUploadSuccess = useCallback(() => {
+    if (window.location.pathname !== UPLOAD_PAGE) {
+      dispatch(
+        toast({
+          content: 'Your upload is complete!',
+          link: UPLOAD_PAGE,
+          linkText: 'View',
+          leftIcon: IconCloudUpload,
+          rightIcon: IconArrowRight
+        })
+      )
+    }
+  }, [dispatch])
+
+  const handleUploadError = useCallback(() => {
+    if (window.location.pathname !== UPLOAD_PAGE) {
+      dispatch(
+        toast({
+          content: 'An error occurred during upload.',
+          link: UPLOAD_PAGE,
+          linkText: 'View',
+          leftIcon: IconCloudUpload,
+          rightIcon: IconArrowRight
+        })
+      )
+    }
+  }, [dispatch])
+
+  const { startUpload, finishUpload } = useUpload(
+    handleUploadSuccess,
+    handleUploadError
+  )
 
   // For navigating back to a remix contest page
   const { data: originalTrack } = useTrack(
