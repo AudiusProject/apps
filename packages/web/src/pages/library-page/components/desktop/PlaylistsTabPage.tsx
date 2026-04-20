c9c130ef01dfc0b028e33a37c5c3cb939da0fb40
julian <me@julianbaker.design>
2026-04-20 17:49:46 -0500

    Make library cards responsive to container width

--- packages/web/src/pages/library-page/components/desktop/PlaylistsTabPage.tsx
+++ packages/web/src/pages/library-page/components/desktop/PlaylistsTabPage.tsx
@@ -70,19 +70,27 @@ export const PlaylistsTabPage = () => {
       <UploadChip
         type='playlist'
         variant='card'
+        cardStyle='fluid'
         source={CreatePlaylistSource.LIBRARY_PAGE}
       />
     )
     const loadedCards = [
       createPlaylistCard,
       ...playlistIds?.map((playlistId) => {
-        return <CollectionCard key={playlistId} id={playlistId} size='m' />
+        return (
+          <CollectionCard key={playlistId} id={playlistId} size='m' w='100%' />
+        )
       })
     ]
     if (!isFetchingNextPage) return loadedCards
     return loadedCards.concat(
       Array.from({ length: 6 }, (_, i) => (
-        <CollectionCardSkeleton key={`loading-${i}`} size='m' noShimmer />
+        <CollectionCardSkeleton
+          key={`loading-${i}`}
+          size='m'
+          w='100%'
+          noShimmer
+        />
       ))
     )
   }, [playlistIds, isFetchingNextPage])
@@ -91,7 +99,7 @@ export const PlaylistsTabPage = () => {
     return (
       <div className={styles.cardsContainer}>
         {Array.from({ length: 12 }, (_, i) => (
-          <CollectionCardSkeleton key={i} size='m' noShimmer />
+          <CollectionCardSkeleton key={i} size='m' w='100%' noShimmer />
         ))}
       </div>
     )
