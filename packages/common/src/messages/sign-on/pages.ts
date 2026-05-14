12efd9fbfcaf62044b75f4cb3cab43157f0cfa0e
julian <me@julianbaker.design>
2026-05-14 12:05:24 -0500

    Update sign up display name placeholder

--- packages/common/src/messages/sign-on/pages.ts
+++ packages/common/src/messages/sign-on/pages.ts
@@ -68,7 +68,7 @@ export const finishProfilePageMessages = {
   description:
     'Your photos & Display Name is how others see you. Customize with special characters, spaces, emojis, whatever!',
   displayName: 'Display Name',
-  inputPlaceholder: 'Display Name',
+  inputPlaceholder: 'Enter your display name',
   goBack: 'Go back',
   profileImageUploadError: 'There was an issue uploading your profile image.',
   coverPhotoUploadError: 'There was an issue uploading your cover photo image.',
